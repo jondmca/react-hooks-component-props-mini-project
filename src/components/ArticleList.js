e73ad545b9437049495e0bd764804eddb2e26263
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2022-06-07 21:30:49 -0500

    Completed

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Article from "./Article";
 
-function ArticleList({articles}){
-    //console.log(articles);
-    const articleElements = articles.map((article) => <Article key={article.id} title={article.title} date={article.date} preview={article.preview} minutes={article.minutes} />);
+function ArticleList({posts}){
+    const articleElements = posts.map((article) => {
+    return <Article key={article.id} title={article.title} date={article.date} preview={article.preview} minutes={article.minutes} />});
 
     return (
         <div>
